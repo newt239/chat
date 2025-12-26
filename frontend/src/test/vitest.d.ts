fcb5860e7aaa663119862243026129cb8c1a8971
newt <ysti2019.win2020.st@gmail.com>
2025-12-26 13:24:23 +0900

    remove: all test code

--- frontend/src/test/vitest.d.ts
+++
@@ -1,9 +0,0 @@
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-import "vitest";
-
-declare module "vitest" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-explicit-any
-  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-explicit-any
-  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<any, void> {}
-}
